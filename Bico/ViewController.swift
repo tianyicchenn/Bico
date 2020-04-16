0ea27df83fea964716a1e0ac63347064031b1b86
Tianyi Chen <tianyichen9828@gmail.com>
2020-04-16 11:29:18 +0800

    Initial Commit

---
+++ Bico/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bico
+//
+//  Created by Tianyi on 2020/4/16.
+//  Copyright © 2020 Tianyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
